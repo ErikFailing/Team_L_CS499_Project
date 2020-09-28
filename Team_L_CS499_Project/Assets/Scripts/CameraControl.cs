30de529ee50b59734943613517df3f23d72d8a9a
Erik Failing <35756586+ewfCode@users.noreply.github.com>
2020-09-28 18:03:47 -0500

    Implemented fully functional camera movement in 2D & 3D
    Implemented the ability to rotate, zoom and move the camera in the 3D and 2D perspective modes.

--- Team_L_CS499_Project/Assets/Scripts/CameraControl.cs
+++ Team_L_CS499_Project/Assets/Scripts/CameraControl.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public bool TwoDMode;
+    public float CamSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        TwoDMode = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Verticle & horizontal movement
+        if (Input.GetAxis("Horizontal") > 0) { transform.position += transform.right * CamSpeed; }
+        else if (Input.GetAxis("Horizontal") < 0) { transform.position -= transform.right * CamSpeed; }
+        if (Input.GetAxis("Vertical") > 0) { transform.position += transform.up * CamSpeed; }
+        else if (Input.GetAxis("Vertical") < 0) { transform.position -= transform.up * CamSpeed; }
+
+
+
+        if (TwoDMode)
+        {
+            // Zoom
+            if (Input.GetAxis("Zoom") > 0) { GetComponent<Camera>().orthographicSize -= CamSpeed * 3; }
+            else if (Input.GetAxis("Zoom") < 0) { GetComponent<Camera>().orthographicSize += CamSpeed * 3; }
+        }
+        else 
+        {
+            // Zoom
+            if (Input.GetAxis("Zoom") > 0) { transform.position += transform.forward * CamSpeed * 8; }
+            else if (Input.GetAxis("Zoom") < 0) { transform.position -= transform.forward * CamSpeed * 8; }
+
+            // Rotation
+            if (Input.GetAxis("Rotate") > 0) 
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                transform.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+
     }
 
     public void SwapCameraViewTo2D()
@@ -27,6 +61,8 @@ public class CameraControl : MonoBehaviour
 
         // Set camera view settings to be 2D
         GetComponent<Camera>().orthographic = true;
+
+        TwoDMode = true;
     }
 
     public void SwapCameraViewTo3D()
@@ -39,6 +75,8 @@ public class CameraControl : MonoBehaviour
 
         // Set camera view settings to be 3D
         GetComponent<Camera>().orthographic = false;
+
+        TwoDMode = false;
     }
 
 }
